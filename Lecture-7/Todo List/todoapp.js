36144957853e239824511b3b97c7572d02c64386
aayusharora <aayush113002@gmail.com>
2018-01-06 15:59:08 +0530

    Added Lecture-8

--- Lecture-7/Todo List/todoapp.js
+++ Lecture-7/Todo List/todoapp.js
@@ -2,6 +2,7 @@
  * Created by aayusharora on 12/31/17.
  */
 
+var global = {};
 window.onload = function() {
     let inp = document.getElementById('inp');
     let result = document.getElementById('result');
@@ -9,8 +10,14 @@ window.onload = function() {
 
     let todoList = [];
     btn.onclick = function() {
-        let todo = inp.value;
-        todoList.push(todo);
+
+        let task = {
+            "task": inp.value,
+            "done": false
+        }
+
+        todoList.push(task);
+        global.todoList = todoList;
         display();
     };
 
@@ -18,10 +25,36 @@ window.onload = function() {
         let finalList = "";
         for(let i=0; i<todoList.length; i++) {
 
-            finalList += `<li>${todoList[i]}</li>`
+            finalList += `<li>
+                            <span>
+                               <input onclick="toggle(this)" id="i${i}" type="checkbox">
+                           </span>
+                            <span id="${i}">${todoList[i].task}</span>
+                          </li>`
         }
         result.innerHTML = "";
         result.innerHTML = finalList;
 
+    }
+
+
+};
+
+function toggle(el){
+    let id = el.id.substr(1);
+    global.todoList[id].done = !global.todoList[id].done;
+    let checked = global.todoList[id].done;
+    strikeOff(checked, id);
+
+}
+
+function strikeOff(check, id){
+    let el = document.getElementById(id);
+    if(check) {
+        console.log(check, id);
+        el.style.textDecoration = 'line-through'
+    }
+    else {
+        el.style.textDecoration = 'none';
     }
 }
